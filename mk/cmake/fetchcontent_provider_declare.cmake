b82249802a3a09b40f1010a22b81c2b41dcbd31a
David Bears <dbear4q@gmail.com>
2026-09-22 16:24:44 -0400

    inject a dependency provider that uses FetchContent

---
+++ mk/cmake/fetchcontent_provider_declare.cmake
@@ -0,0 +1,187 @@
+# ------------------------------------------------------------------------ *\
+# mk/cmake/fetchcontent_provider_declare.cmake
+# This file is part of libRetroShare.
+#
+# Copyright (C) 2026      David Bears <dbear4q@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ------------------------------------------------------------------------ */
+
+
+list(APPEND CMAKE_MODULE_PATH
+	"${PROJECT_SOURCE_DIR}/mk/cmake/fetchcontent_provider_packages"
+)
+set(FETCHCONTENT_QUIET OFF)
+include(FetchContent)
+include(ExternalProject)
+
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 4.2)
+	function(EnvironmentModification)
+	endfunction()
+elseif(CMAKE_VERSION VERSION_GREATER_EQUAL 3.25)
+	function(EnvironmentModification outvar)
+		list(TRANSFORM ARGN PREPEND --modify\;)
+		set(${outvar} ${CMAKE_COMMAND} -E env ${ARGN} -- PARENT_SCOPE)
+	endfunction(EnvironmentModification)
+else()
+	function(EnvironmentModification outvar)
+		list(TRANSFORM ARGN REPLACE "^([^=]+)=set:(.*)\$" "\\1=\\2")
+		list(TRANSFORM ARGN REPLACE "^([^=]+)=unset:(.*)\$" "--unset=\\1")
+		set(${outvar} ${CMAKE_COMMAND} -E env ${ARGN} -- PARENT_SCOPE)
+	endfunction(EnvironmentModification)
+endif()
+
+################################################################################
+### bitdht
+
+FetchContent_Declare(bitdht
+	GIT_REPOSITORY "https://github.com/RetroShare/BitDHT.git"
+	GIT_TAG "origin/master"
+	GIT_SHALLOW TRUE
+	GIT_PROGRESS TRUE
+	TIMEOUT 10
+	EXCLUDE_FROM_ALL
+)
+list(APPEND fetchcontent_provider_packages bitdht)
+
+################################################################################
+### cpptrace
+
+FetchContent_Declare(cpptrace
+	GIT_REPOSITORY https://github.com/jeremy-rifkin/cpptrace.git
+	GIT_TAG v0.3.1
+	GIT_SHALLOW TRUE
+	GIT_PROGRESS TRUE
+	TIMEOUT 10
+)
+list(APPEND fetchcontent_provider_packages cpptrace)
+
+################################################################################
+### jni.hpp
+
+FetchContent_Declare(jni.hpp
+	GIT_REPOSITORY "https://github.com/RetroShare/jni.hpp.git"
+	GIT_TAG "origin/master"
+	GIT_SHALLOW TRUE
+	GIT_PROGRESS TRUE
+	TIMEOUT 10
+)
+list(APPEND fetchcontent_provider_packages jni.hpp)
+
+################################################################################
+### openpgpsdk
+
+FetchContent_Declare(openpgpsdk
+	GIT_REPOSITORY "https://github.com/RetroShare/OpenPGP-SDK.git"
+	GIT_TAG "origin/master"
+	GIT_SHALLOW TRUE
+	GIT_PROGRESS TRUE
+	TIMEOUT 10
+	EXCLUDE_FROM_ALL
+)
+list(APPEND fetchcontent_provider_packages openpgpsdk)
+
+################################################################################
+### rapidjson
+
+set(RAPIDJSON_BUILD_EXAMPLES OFF CACHE BOOL "Build rapidjson examples.")
+set(RAPIDJSON_BUILD_TESTS OFF CACHE BOOL
+	"Build rapidjson perftests and unittests."
+)
+FetchContent_Declare(RapidJSON
+	GIT_REPOSITORY "https://github.com/Tencent/rapidjson.git"
+	GIT_TAG "origin/master"
+	GIT_SHALLOW TRUE
+	GIT_PROGRESS TRUE
+	TIMEOUT 10
+	EXCLUDE_FROM_ALL
+)
+list(APPEND fetchcontent_provider_packages RapidJSON)
+
+################################################################################
+### restbed
+
+set(BUILD_TESTS OFF CACHE BOOL "build restbed tests")
+set(BUILD_SSL OFF CACHE BOOL "enable restbed SSL support")
+FetchContent_Declare(restbed
+	GIT_REPOSITORY "https://github.com/Corvusoft/restbed.git"
+	GIT_TAG 6001a322809b5005b8bcccdf593fdda6f0173691
+	GIT_SUBMODULES dependency/asio dependency/catch
+	# GIT_SHALLOW TRUE
+	GIT_PROGRESS TRUE
+	TIMEOUT 10
+)
+list(APPEND fetchcontent_provider_packages restbed)
+
+################################################################################
+### rnp
+
+FetchContent_Declare(
+	rnp
+	GIT_REPOSITORY "https://github.com/rnpgp/rnp.git"
+	GIT_TAG "origin/main"
+	GIT_SHALLOW TRUE
+	GIT_PROGRESS TRUE
+	TIMEOUT 10
+	EXCLUDE_FROM_ALL
+)
+list(APPEND fetchcontent_provider_packages rnp)
+
+################################################################################
+### sam3
+
+set(sam3_external_SOURCE_DIR "sam3_external-prefix/src/sam3_external")
+set(sam3_external_BINARY_DIR ${sam3_external_SOURCE_DIR})
+list(APPEND sam3_external_BUILD_ENVIRONMENT
+  "CC=set:${CMAKE_C_COMPILER}"
+	"AR=set:${CMAKE_AR}"
+)
+if(WIN32)
+  list(APPEND sam3_external_BUILD_ENVIRONMENT
+    "LDFLAGS=set:-lmingw32 -lws2_32 -lwsock32 -mwindows"
+  )
+endif()
+EnvironmentModification(envmod_build ${sam3_external_BUILD_ENVIRONMENT})
+ExternalProject_Add(sam3_external
+	GIT_REPOSITORY "https://github.com/i2p/libsam3.git"
+	GIT_TAG "origin/master"
+	GIT_SHALLOW TRUE
+	GIT_PROGRESS TRUE
+	TIMEOUT 10
+	CONFIGURE_COMMAND ""
+  BUILD_COMMAND ${envmod_build} make build
+  INSTALL_COMMAND ""
+  BUILD_IN_SOURCE TRUE
+	BUILD_ALWAYS TRUE
+	EXCLUDE_FROM_ALL YES
+  BUILD_BYPRODUCTS "${sam3_external_BINARY_DIR}/libsam3.a"
+  BUILD_ENVIRONMENT_MODIFICATION ${sam3_external_BUILD_ENVIRONMENT}
+)
+list(APPEND fetchcontent_provider_packages sam3_external)
+
+################################################################################
+### udp-discovery
+
+## TODO: upstream option to disable tests building
+set(BUILD_EXAMPLE OFF CACHE BOOL "build udp-discovery-cpp examples")
+set(BUILD_TOOL OFF CACHE BOOL "build udp-discovery-tool application")
+FetchContent_Declare(udp-discovery-cpp
+	GIT_REPOSITORY "https://github.com/truvorskameikin/udp-discovery-cpp.git"
+	GIT_TAG "origin/master"
+	GIT_SHALLOW TRUE
+	GIT_PROGRESS TRUE
+	TIMEOUT 10
+	PATCH_COMMAND sed -i -e "s/^cmake_minimum_required(VERSION 3.0)\$/cmake_minimum_required(VERSION 2.8...4.3.4)/" CMakeLists.txt
+)
+list(APPEND fetchcontent_provider_packages udp-discovery-cpp)
